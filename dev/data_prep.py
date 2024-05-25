f82c440d1d7fef0a51b9aad30566c1c6a5a27687
ESCM <37626503+escmHEX@users.noreply.github.com>
2024-05-25 15:59:12 -0400

    Update data_prep.py

--- dev/data_prep.py
+++ dev/data_prep.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import cupy as cp
-from skimage.transform import resize
 
 class Dataset:
-    def __init__(self, path):
+    def __init__(self, path, isTrainAndVal=False):
         self.data = pd.read_csv(path).to_numpy()
         self.samples = self.data.shape[0]
         self.features = self.data[:self.samples, 1:-1]
-    
         self.labels = self.data[:self.samples, -1]
-        self.labels = self.labels.reshape(-1, 1)
+        self.labels = self.labels.reshape(1, -1).T 
         
     def X_data(self):
         return cp.array(self.features)
@@ -17,4 +15,32 @@ class Dataset:
     def Y_data(self):
         return cp.array(self.labels).flatten() 
 
+class TrainAndVal:
+    def __init__(self, path, train_percentage):
+        self.data = pd.read_csv(path).to_numpy()
+        self.samples = self.data.shape[0]
+        self.train_samples = int(self.samples * train_percentage)
+        
+        self.train_set = {
+            'features': self.data[:self.train_samples, 1:-1],
+            'labels': (self.data[:self.train_samples, -1]).reshape(1, -1).T
+        }
+        
+        self.val_set= {
+            'features': self.data[:(self.samples - self.train_samples), 1:-1],
+            'labels': (self.data[:(self.samples - self.train_samples ), -1]).reshape(1, -1).T
+        }
+        
+    def get_train_data(self):
+        return {
+            'X': cp.array(self.train_set['features']),
+            'Y': cp.array(self.train_set['labels']).flatten()
+        }
+        
+    def get_val_data(self):
+        return {
+            'X': cp.array(self.val_set['features']),
+            'Y': cp.array(self.val_set['labels']).flatten()
+        }
+        
 
